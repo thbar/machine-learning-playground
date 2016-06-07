f16902e7e3354b3f96f76adde73744b741ea1495
Thibaut Barrère <thibaut.barrere@gmail.com>
2016-06-07 11:01:12 +0200

    Add a sample matrix test case

--- koans.m
+++ koans.m
@@ -18,3 +18,14 @@ function test_matrix_multiplication
     ],
     a * b
   );
+
+function test_matrix_size
+  a = [ 1 2  3 ; 4 5 6];
+  % size is a matrix
+  assertEqual([ 2 3 ], size(a))
+  % we can query a specific dimension
+  assertEqual(3, size(a, 2))
+  % the "length" is the longest dimension
+  assertEqual(3, length(a))
+  % which works conveniently for a vector
+  assertEqual(4, length([1 2 3 4]))
